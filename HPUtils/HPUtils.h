880c188e5a16a14af500d27e886d470d072bf267
Herve Peroteau <herve.peroteau@gmail.com>
2013-12-11 07:03:12 +0100

    Initial Commit

---
+++ HPUtils/HPUtils.h
@@ -0,0 +1,13 @@
+//
+//  HPUtils.h
+//  HPUtils
+//
+//  Created by Hervé PEROTEAU on 11/12/2013.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HPUtils : NSObject
+
+@end
---
+++ HPUtils/HPUtils.m
@@ -0,0 +1,13 @@
+//
+//  HPUtils.m
+//  HPUtils
+//
+//  Created by Hervé PEROTEAU on 11/12/2013.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import "HPUtils.h"
+
+@implementation HPUtils
+
+@end
